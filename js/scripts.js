43e2446a2cde9b306d29877411e434a9ff7ebcdb
AzeemNasir007 <azeem_nasir@outlook.com>
2022-05-11 11:48:22 -0700

    Update scripts.js
    Added new variable pokemonList
    Added several objects to the array

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList= [ 
+  {name: "Gastly", Height: "1.3", Type: ["Ghost","Poison"]},
+  {name: "Haunter", Height: "1.6", Type: ["Ghost","Poison"]},
+  {name: "Gengar", Height: "1.5", Type: ["Ghost","Poison"]}
+];
