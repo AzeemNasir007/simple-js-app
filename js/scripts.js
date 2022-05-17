b8b5766dda65199d22f9d6087cd9e12f3e6db9af
AzeemNasir007 <azeem_nasir@outlook.com>
2022-05-17 13:38:23 -0700

    Update scripts.js
    switched out a pokemon to create more differences, added a (for)loop with comments for my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 let pokemonList= [ 
   {name: "Gastly", Height: "1.3", Type: ["Ghost","Poison"]},
   {name: "Haunter", Height: "1.6", Type: ["Ghost","Poison"]},
-  {name: "Gengar", Height: "1.5", Type: ["Ghost","Poison"]}
+  {name: "Bulbasaur", Height: "0.7", Type: ["Grass","Poison"]}
 ];
+
+//Goes through the pokemon list until all the items are reached
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<br>' + pokemonList[i].name + ' ' + pokemonList[i].Height + ' ');
+
+  //If criteria is met, pokemon would have a comment next to it
+  if (pokemonList[i].Height >= 1.4) {
+    document.write(' - Wow that is quite big!! ');
+  }
+}
+
+
+
+
