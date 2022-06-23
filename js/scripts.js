9aae775fc44208ecc491de215932b13f90f79719
AzeemNasir007 <azeem_nasir@outlook.com>
2022-06-23 12:21:18 -0700

    Update scripts.js
    organizing and changing for loop to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 let pokemonList= [ 
-  {name: "Gastly", Height: "1.3", Type: ["Ghost","Poison"]},
-  {name: "Haunter", Height: "1.6", Type: ["Ghost","Poison"]},
-  {name: "Bulbasaur", Height: "0.7", Type: ["Grass","Poison"]}
+  {
+    name: "Gastly", 
+    height: "1.3", 
+    type: ["Ghost","Poison"]
+  },
+  {
+    name: "Haunter", 
+    height: "1.6", 
+    type: ["Ghost","Poison"]
+  },
+  {
+    name: "Bulbasaur", 
+    height: "0.7", 
+    type: ["Grass","Poison"]
+  }
 ];
 
-//Goes through the pokemon list until all the items are reached
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write('<br>' + pokemonList[i].name + ' ' + pokemonList[i].Height + ' ');
 
-  //If criteria is met, pokemon would have a comment next to it
-  if (pokemonList[i].Height >= 1.4) {
-    document.write(' - Wow that is quite big!! ');
-  }
-}
-
-
-
-
+pokemonList.forEach(function(pokemon) {
+  console.log(pokemon.name + ' is ' + pokemon.Height + ' tall.');
+});
