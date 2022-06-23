e3b515b9bfe5310dce51cc0583818e4d3ae8f06e
AzeemNasir007 <azeem_nasir@outlook.com>
2022-06-23 12:25:26 -0700

    Update scripts.js
    cleaning up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
-let pokemonList= [ 
+let pokemonList = [ 
   {
-    name: "Gastly", 
-    height: "1.3", 
-    type: ["Ghost","Poison"]
+    name: 'Gastly', 
+    height: '1.3', 
+    type: ['Ghost','Poison']
   },
   {
-    name: "Haunter", 
-    height: "1.6", 
-    type: ["Ghost","Poison"]
+    name: 'Haunter', 
+    height: '1.6', 
+    type: ['Ghost','Poison']
   },
   {
-    name: "Bulbasaur", 
-    height: "0.7", 
-    type: ["Grass","Poison"]
+    name: 'Bulbasaur', 
+    height: '0.7', 
+    type: ['Grass','Poison']
   }
 ];
 
-
 pokemonList.forEach(function(pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.Height + ' tall.');
+  console.log(pokemon.name + ' is ' + pokemon.height + ' tall.');
 });
