d325951915ec9490ef98c038da5e1d83f712a2e9
AzeemNasir007 <azeem_nasir@outlook.com>
2022-06-25 20:52:48 -0700

    Update scripts.js
    added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,42 @@
-let pokemonList = [ 
-  {
-    name: 'Gastly', 
-    height: '1.3', 
-    type: ['Ghost','Poison']
-  },
-  {
-    name: 'Haunter', 
-    height: '1.6', 
-    type: ['Ghost','Poison']
-  },
-  {
-    name: 'Bulbasaur', 
-    height: '0.7', 
-    type: ['Grass','Poison']
+let pokemonRepository = (function () {
+
+  let pokemonList = [ 
+    {
+      name: 'Gastly', 
+      height: 1.3, 
+      type: ['Ghost','Poison']
+    },
+    {
+      name: 'Haunter', 
+      height: 1.6, 
+      type: ['Ghost','Poison']
+    },
+    {
+      name: 'Bulbasaur', 
+      height: 0.7, 
+      type: ['Grass','Poison']
+    }
+  ]
+
+  function getAll () {
+    return pokemonList;
   }
-];
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  }
+
+})()
+
+console.log(pokemonRepository.getAll())
+pokemonRepository.add({ name: 'Pikachu' });
+console.log(pokemonRepository.getAll()); 
+
 
 pokemonList.forEach(function(pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + ' tall.');
+  console.log(pokemon.name + ' ' + pokemon.height);
 });
